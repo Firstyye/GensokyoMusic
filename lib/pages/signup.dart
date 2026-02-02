a55ba497f556493b523fb0b194bb0b8a48e938a1
Firstyye <Firsty5148@gmail.com>
2026-02-03 03:13:20 +0700

    fixed whatever this is

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -83,7 +83,10 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final viewInsets = MediaQuery.of(context).viewInsets;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Positioned(
@@ -130,176 +133,181 @@ class _SignupScreenState extends State<SignupScreen> {
             ),
           ),
 
-          SingleChildScrollView(
-            child: Stack(
-              
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SafeArea(
-                        child: Image(
-                          width: 350,
-                          height: 300,
-                          image: AssetImage('assets/images/CirnoLogin.png'),
-                        ),
-                      ),
-            
-                      Text(
-                        "Ready to get started?",
-                        style: headerTextStyle.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        "Create an account to start your musical journey now!",
-                        textAlign: TextAlign.center,
-                        style: bodyTextStyle.copyWith(
-                          fontSize: 12.5,
-                          fontWeight: FontWeight.w100,
-                          color: Colors.grey.withOpacity(0.9),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      _BuildTextField(
-                        controller: _usernameController,
-                        labelText: "Username",
-                        isObscureText: false,
-                        isPassword: false,
-                        icon: Icons.person,
-                      ),
-                      _BuildTextField(
-                        controller: _emailController,
-                        labelText: "Email",
-                        isObscureText: false,
-                        isPassword: false,
-                        typeText: false,
-                        icon: Icons.email,
-                      ),
-            
-                      _BuildTextField(
-                        controller: _passwordController,
-                        labelText: "Password",
-                        isObscureText: true,
-                        isPassword: true,
-                        icon: Icons.lock,
-                      ),
-                      _BuildTextField(
-                        controller: _confirmpasswordController,
-                        labelText: "Confirm Password",
-                        isObscureText: true,
-                        isPassword: true,
-                        icon: Icons.lock_reset,
-                      ),
-            
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            backgroundColor: Colors.blueAccent,
-                            foregroundColor: Colors.white,
-                          ),
-            
-                          child: Text(
-                            "SIGN UP",
-                            style: bodyTextStyle.copyWith(
-                              fontSize: 16,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+          Positioned.fill(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: viewInsets.bottom),
+                child: Stack( 
+                  
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
+                          SafeArea(
+                            child: Image(
+                              width: 350,
+                              height: 300,
+                              image: AssetImage('assets/images/CirnoLogin.png'),
+                            ),
+                          ),
+                
                           Text(
-                            "Already have an account?",
-            
+                            "Ready to get started?",
                             style: headerTextStyle.copyWith(
-                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            "Create an account to start your musical journey now!",
+                            textAlign: TextAlign.center,
+                            style: bodyTextStyle.copyWith(
+                              fontSize: 12.5,
+                              fontWeight: FontWeight.w100,
                               color: Colors.grey.withOpacity(0.9),
                             ),
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(
-                              " Login ",
-                              style: headerTextStyle.copyWith(
-                                fontSize: 10,
-                                color: Colors.blueAccent,
+                          SizedBox(height: 20),
+                          _BuildTextField(
+                            controller: _usernameController,
+                            labelText: "Username",
+                            isObscureText: false,
+                            isPassword: false,
+                            icon: Icons.person,
+                          ),
+                          _BuildTextField(
+                            controller: _emailController,
+                            labelText: "Email",
+                            isObscureText: false,
+                            isPassword: false,
+                            typeText: false,
+                            icon: Icons.email,
+                          ),
+                
+                          _BuildTextField(
+                            controller: _passwordController,
+                            labelText: "Password",
+                            isObscureText: true,
+                            isPassword: true,
+                            icon: Icons.lock,
+                          ),
+                          _BuildTextField(
+                            controller: _confirmpasswordController,
+                            labelText: "Confirm Password",
+                            isObscureText: true,
+                            isPassword: true,
+                            icon: Icons.lock_reset,
+                          ),
+                
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 50),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                backgroundColor: Colors.blueAccent,
+                                foregroundColor: Colors.white,
+                              ),
+                
+                              child: Text(
+                                "SIGN UP",
+                                style: bodyTextStyle.copyWith(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
-                          Text(
-                            "Here",
-                            style: headerTextStyle.copyWith(
-                              fontSize: 10,
-                              color: Colors.grey.withOpacity(0.9),
-                            ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Already have an account?",
+                
+                                style: headerTextStyle.copyWith(
+                                  fontSize: 10,
+                                  color: Colors.grey.withOpacity(0.9),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  " Login ",
+                                  style: headerTextStyle.copyWith(
+                                    fontSize: 10,
+                                    color: Colors.blueAccent,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "Here",
+                                style: headerTextStyle.copyWith(
+                                  fontSize: 10,
+                                  color: Colors.grey.withOpacity(0.9),
+                                ),
+                              ),
+                            ],
                           ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(child: Divider(thickness: 1, endIndent: 10)),
+                              Text(
+                                "OR CONTINUE WITH",
+                                style: headerTextStyle.copyWith(
+                                  fontSize: 10,
+                                  color: Colors.grey.withOpacity(0.9),
+                                ),
+                              ),
+                
+                              Expanded(child: Divider(thickness: 1, indent: 10)),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              _LoginButton(
+                                assetPath: "assets/icons/android_neutral_rd_na.svg",
+                                semanticsLabel: 'Google logo',
+                              ),
+                              _LoginButton(
+                                icon: FontAwesomeIcons.github,
+                                iconSize: 24,
+                                semanticsLabel: 'Github logo',
+                              ),
+                              _LoginButton(
+                                icon: FontAwesomeIcons.facebook,
+                                iconSize: 24,
+                                iconColor: Colors.blue[700],
+                                semanticsLabel: 'Facebook logo',
+                              ),
+                              _LoginButton(
+                                icon: FontAwesomeIcons.twitter,
+                                iconSize: 24,
+                                iconColor: Colors.blue,
+                                semanticsLabel: 'Twitter logo',
+                              ),
+                            ],
+                          ),
+                         
                         ],
                       ),
-                      SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(child: Divider(thickness: 1, endIndent: 10)),
-                          Text(
-                            "OR CONTINUE WITH",
-                            style: headerTextStyle.copyWith(
-                              fontSize: 10,
-                              color: Colors.grey.withOpacity(0.9),
-                            ),
-                          ),
-            
-                          Expanded(child: Divider(thickness: 1, indent: 10)),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          _LoginButton(
-                            assetPath: "assets/icons/android_neutral_rd_na.svg",
-                            semanticsLabel: 'Google logo',
-                          ),
-                          _LoginButton(
-                            icon: FontAwesomeIcons.github,
-                            iconSize: 24,
-                            semanticsLabel: 'Github logo',
-                          ),
-                          _LoginButton(
-                            icon: FontAwesomeIcons.facebook,
-                            iconSize: 24,
-                            iconColor: Colors.blue[700],
-                            semanticsLabel: 'Facebook logo',
-                          ),
-                          _LoginButton(
-                            icon: FontAwesomeIcons.twitter,
-                            iconSize: 24,
-                            iconColor: Colors.blue,
-                            semanticsLabel: 'Twitter logo',
-                          ),
-                        ],
-                      ),
-                     
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
